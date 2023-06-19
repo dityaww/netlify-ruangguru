d9ced0958b73ea92b2dc0d05afe8497b76ad9691
dityaww <111202012838@mhs.dinus.ac.id>
2023-06-19 09:30:51 +0700

    project-netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Aditya Widyatmoko"; // TODO: replace this
+const studentId = "FE5409078"; // TODO: replace this
+
+export { url, studentName, studentId };
